46e872d0efe4ebff75f1dbd2c76704218c24311b
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2019-02-07 15:53:25 -0500

    blinkign stuff

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -16,11 +16,12 @@ public class PlayerManager : MonoBehaviour
     public ParticleSystem deathParticle;
     public GameObject respawnPlatform;
     public Material material;
+    private int blinkingDelay;
 
     // Start is called before the first frame update
     void Start()
     {
-        // stop
+        blinkingDelay = 0;
     }
 
     // Update is called once per frame
@@ -47,16 +48,16 @@ public class PlayerManager : MonoBehaviour
     private IEnumerator Respawn()
     {
         //Make it flashing
-        StartCoroutine(Flash());
+        InvokeRepeating("blink", 0, 0.16f);
 
         state = PlayerState.Respawning;
         yield return new WaitForSeconds(2);
-        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None | 
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None |
             RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX;
         GameObject platform = Instantiate(respawnPlatform, new Vector3(0, 6, -5.45f), Quaternion.identity);
-        transform.position = platform.transform.position + new Vector3(0,1,0);
+        transform.position = platform.transform.position + new Vector3(0, 1, 0);
         platform.GetComponent<Rigidbody>().isKinematic = false;
-        
+
         for (int i = 0; i < 30; i++)
         {
             yield return new WaitForFixedUpdate();
@@ -72,20 +73,26 @@ public class PlayerManager : MonoBehaviour
         Destroy(platform);
 
         state = PlayerState.Alive;
-
+        CancelInvoke("blink");
         //Make them not colide
 
 
+
     }
 
+    void blink()
+    {
+        Invoke("goTransparent", 0f);
+        Invoke("goUntransparent", 0.08f);
+    }
 
-
-    private IEnumerator Flash()
+    void goTransparent()
     {
         material.color = new Color(material.color.r, material.color.g, material.color.b, 1f);
-        yield return new WaitForSecondsRealtime(5);
-        material.color = new Color(material.color.r, material.color.g, material.color.b, 1);
-
     }
 
+    void goUntransparent()
+    {
+        material.color = new Color(material.color.r, material.color.g, material.color.b, 0f);
+    }
 }
