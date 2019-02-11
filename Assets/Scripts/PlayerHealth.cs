de49275915f9e5f1f29fe9399d87497c61028c6c
yuviiii <yitian.ding@mail.utoronto.ca>
2019-02-10 19:26:33 -0500

    add health UI

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int startingHealth = 0;                            // The amount of health the player starts the game with.
+    public int currentHealth;                                   // The current health the player has.
+    public Slider healthSlider;                                 // Reference to the UI's health bar.
+    public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+    public AudioClip deathClip;                                 // The audio clip to play when the player dies.
+    public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+
+
+    Animator anim;                                              // Reference to the Animator component.
+    AudioSource playerAudio;                                    // Reference to the AudioSource component.
+    Player playerMovement;                              // Reference to the player's movement.
+    //PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
+    //bool isDead;                                                // Whether the player is dead.
+    bool damaged;                                               // True when the player gets damaged.
+
+
+    void Awake()
     {
-        
+        // Setting up the references.
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<Player>();
+        //playerShooting = GetComponentInChildren<PlayerShooting>();
+
+        // Set the initial health of the player.
+        currentHealth = startingHealth;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        // If the player has just been damaged...
+        if (damaged)
+        {
+            // ... set the colour of the damageImage to the flash colour.
+            damageImage.color = flashColour;
+        }
+        // Otherwise...
+        else
+        {
+            // ... transition the colour back to clear.
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        // Reset the damaged flag.
+        damaged = false;
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        // Set the damaged flag so the screen will flash.
+        damaged = true;
+
+        // Reduce the current health by the damage amount.
+        currentHealth += amount;
+
+        // Set the health bar's value to the current health.
+        healthSlider.value = currentHealth;
+
+        // Play the hurt sound effect.
+        playerAudio.Play();
+
+        // If the player has lost all it's health and the death flag hasn't been set yet...
+        //if (currentHealth >= 0 && !isDead)
+        //{
+        //    // ... it should die.
+        //    Death();
+        //}
     }
 }
