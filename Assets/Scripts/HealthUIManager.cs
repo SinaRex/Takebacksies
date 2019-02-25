0ec4b53db0a43999371e8056be795378892ef383
Seong Kim <hoon5421@hotmail.com>
2019-02-24 21:19:32 -0500

    Temporary fix for p2 health

--- Assets/Scripts/HealthUIManager.cs
+++ Assets/Scripts/HealthUIManager.cs
@@ -9,12 +9,6 @@ public class HealthUIManager : MonoBehaviour
     private int p1health, p2health;
     private GameManger gameManger;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Get p1,p2 health;
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -27,52 +21,55 @@ public class HealthUIManager : MonoBehaviour
         p1health = gameManger.playersLives[0];
         p2health = gameManger.playersLives[1];
 
-        switch (p1health)
+        //Temporary switching to if statements for quick fix
+        if(p1health == 3)
         {
-            case 3:
-                p1life1.gameObject.SetActive(true);
-                p1life2.gameObject.SetActive(true);
-                p1life3.gameObject.SetActive(true);
-                return;
-            case 2:
-                p1life1.gameObject.SetActive(true);
-                p1life2.gameObject.SetActive(true);
-                p1life3.gameObject.SetActive(false);
-                return;
-            case 1:
-                p1life1.gameObject.SetActive(true);
-                p1life2.gameObject.SetActive(false);
-                p1life3.gameObject.SetActive(false);
-                return;
-            case 0:
-                p1life1.gameObject.SetActive(false);
-                p1life2.gameObject.SetActive(false);
-                p1life3.gameObject.SetActive(false);
-                return;
-
+            p1life1.gameObject.SetActive(true);
+            p1life2.gameObject.SetActive(true);
+            p1life3.gameObject.SetActive(true);
         }
-        switch (p2health)
+        else if(p1health == 2)
         {
-            case 3:
-                p2life1.gameObject.SetActive(true);
-                p2life2.gameObject.SetActive(true);
-                p2life3.gameObject.SetActive(true);
-                return;
-            case 2:
-                p2life1.gameObject.SetActive(true);
-                p2life2.gameObject.SetActive(true);
-                p2life3.gameObject.SetActive(false);
-                return;
-            case 1:
-                p2life1.gameObject.SetActive(true);
-                p2life2.gameObject.SetActive(false);
-                p2life3.gameObject.SetActive(false);
-                return;
-            case 0:
-                p2life1.gameObject.SetActive(false);
-                p2life2.gameObject.SetActive(false);
-                p2life3.gameObject.SetActive(false);
-                return;
+            p1life1.gameObject.SetActive(true);
+            p1life2.gameObject.SetActive(true);
+            p1life3.gameObject.SetActive(false);
+        }
+        else if(p1health == 1)
+        {
+            p1life1.gameObject.SetActive(true);
+            p1life2.gameObject.SetActive(false);
+            p1life3.gameObject.SetActive(false);
+        }
+        else if(p1health == 0)
+        {
+            p1life1.gameObject.SetActive(false);
+            p1life2.gameObject.SetActive(false);
+            p1life3.gameObject.SetActive(false);
+        }
+
+        if (p2health == 3)
+        {
+            p2life1.gameObject.SetActive(true);
+            p2life2.gameObject.SetActive(true);
+            p2life3.gameObject.SetActive(true);
+        }
+        else if (p2health == 2)
+        {
+            p2life1.gameObject.SetActive(true);
+            p2life2.gameObject.SetActive(true);
+            p2life3.gameObject.SetActive(false);
+        }
+        else if (p2health == 1)
+        {
+            p2life1.gameObject.SetActive(true);
+            p2life2.gameObject.SetActive(false);
+            p2life3.gameObject.SetActive(false);
+        }
+        else if (p2health == 0)
+        {
+            p2life1.gameObject.SetActive(false);
+            p2life2.gameObject.SetActive(false);
+            p2life3.gameObject.SetActive(false);
         }
     }
 
