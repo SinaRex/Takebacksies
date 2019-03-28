55d7f6230962f200d2a56c23f5ae2149e3827be5
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2019-03-28 18:21:25 -0400

    cavestage is getting polished

---
+++ Assets/Scripts/flickerFire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flickerFire : MonoBehaviour
+{
+    private float maxFlickerTimer;
+    private float flickerCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flickerCounter = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (flickerCounter >= maxFlickerTimer)
+        {
+            gameObject.GetComponent<Light>().intensity = Random.Range(0.5f,1.2f);
+            maxFlickerTimer = Random.Range(1.5f, 3);
+            flickerCounter = 0;
+        }
+        flickerCounter += 1;
+
+    }
+}
