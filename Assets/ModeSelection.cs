2aedf4910e2f4d76b64e2332a6ddf94370e421ff
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2019-04-16 22:17:03 -0400

    start screen almost done

---
+++ Assets/ModeSelection.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModeSelection : MonoBehaviour
+{
+    //public GameObject mode1;
+    //public GameObject mode2;
+    //public GameObject mode3;
+
+    public Text mode1Text;
+    public Text mode2Text;
+    public Text mode3Text;
+    public Text mode4Text;
+
+
+    public GameObject actualSun;
+    public GameObject sunSelector;
+
+    private int selected = -1;
+    private float inputDelay = 0.15f;
+    private float nextInput = 0f;
+    private bool deBounce = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!deBounce)
+        {
+            if (Input.GetAxisRaw("MoveAxisY1") > 0 || Input.GetAxis("MoveAxisY2") > 0 || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                deBounce = true;
+                switch (selected)
+                {
+                    case -1:
+                        selected = 3;
+                        break;
+                    case 0:
+                        selected = 3;
+                        break;
+                    case 1:
+                        selected = 0;
+                        break;
+                    case 2:
+                        selected = 1;
+                        break;
+                    case 3:
+                        selected = 2;
+                        break;
+                    default:
+                        selected = -1;
+                        break;
+                }
+
+
+            }
+
+            if (Input.GetAxisRaw("MoveAxisY1") < 0 || Input.GetAxis("MoveAxisY2") < 0 || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                deBounce = true;
+                switch (selected)
+                {
+                    case -1:
+                        selected = 0;
+                        break;
+                    case 0:
+                        selected = 1;
+                        break;
+                    case 1:
+                        selected = 2;
+                        break;
+                    case 2:
+                        selected = 3;
+                        break;
+                    case 3:
+                        selected = 0;
+                        break;
+                    default:
+                        selected = -1;
+                        break;
+                }
+
+
+            }
+        }
+
+        OnSelectChange(selected);
+
+    }
+
+    void OnSelectChange(int index)
+    {
+        if (index != -1)
+        {
+            actualSun.SetActive(false);
+            sunSelector.SetActive(true);
+        }
+
+        switch (index)
+        {
+            case 0:
+                SelectText(mode1Text);
+                ResetText(mode2Text);
+                ResetText(mode3Text);
+                ResetText(mode4Text);
+                sunSelector.transform.position = Vector3.Lerp();
+                break;
+            case 1:
+                ResetText(mode1Text);
+                SelectText(mode2Text);
+                ResetText(mode3Text);
+                ResetText(mode4Text);
+                break;
+            case 2:
+                ResetText(mode1Text);
+                ResetText(mode2Text);
+                SelectText(mode3Text);
+                ResetText(mode4Text);
+                break;
+            case 3:
+                ResetText(mode1Text);
+                ResetText(mode2Text);
+                ResetText(mode3Text);
+                SelectText(mode4Text);
+                break;
+        }
+        StartCoroutine(ResetDebounce());
+    }
+
+    void ResetText(Text text)
+    {
+        text.fontSize = (int) Mathf.Lerp(text.fontSize, 24f, 0.8f);
+        text.rectTransform.sizeDelta = Vector2.Lerp(text.rectTransform.sizeDelta, 
+            new Vector2(text.rectTransform.sizeDelta.x, 50), 0.8f);
+    }
+
+    void SelectText(Text text)
+    {
+        text.fontSize = (int)Mathf.Lerp(text.fontSize, 46f, 0.8f);
+        text.rectTransform.sizeDelta = Vector2.Lerp(text.rectTransform.sizeDelta,
+        new Vector2(text.rectTransform.sizeDelta.x, 100), 0.8f);
+
+    }
+
+    IEnumerator ResetDebounce()
+    {
+        yield return new WaitForSeconds(0f);
+        if (Input.GetAxisRaw("MoveAxisY1") == 0)
+        {
+            deBounce = false;
+        }
+    }
+}
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -7,7 +7,15 @@ public class StartGame : MonoBehaviour
 {
     public GameObject foreGround;
     public GameObject backGround;
+    public GameObject mainCam;
 
+    /*Canvas stuff*/
+    public GameObject logo;
+    public GameObject startButton;
+    public GameObject canvas1;
+    public GameObject canvas2;
+
+    private bool lerpCamera = false;
 
     void Start() {
         Time.timeScale = 1f;
@@ -23,6 +31,15 @@ public class StartGame : MonoBehaviour
             ChangeScene();
         }
 
+        // Pan the camera up to the mode selection screen
+        if (lerpCamera)
+        {
+            Vector3 toLerpPos = new Vector3(mainCam.transform.position.x, 42, mainCam.transform.position.z);
+            mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, toLerpPos, 0.1f);
+
+        }
+
+
     }
 
     public void ChangeScene()
@@ -35,7 +52,20 @@ public class StartGame : MonoBehaviour
         backGround.GetComponent<Animator>().SetTrigger("backStart");
         yield return new WaitForSeconds(0.2f);
         foreGround.GetComponent<Animator>().SetTrigger("ForeStart");
-        yield return new WaitForSeconds(2.0f);
-        SceneManager.LoadScene("MapSelectScene");
+        logo.GetComponent<Animator>().SetTrigger("GoMode");
+        startButton.GetComponent<Animator>().SetTrigger("GoMode");
+        lerpCamera = true;
+        yield return new WaitForSeconds(1f);
+        canvas1.SetActive(false);
+        canvas2.SetActive(true);
+           
+        // FOR THE REST OF THE CODE LOOK AT THE SCRIPT ATTACHED TO Canvas2
+
+
+
+        yield return new WaitForSeconds(0f);
+
+
+
     }
 }
